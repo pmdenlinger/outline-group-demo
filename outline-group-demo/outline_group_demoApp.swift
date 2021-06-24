62f22e2d6901dc7d8fc915faadd51aaed52b93b1
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-23 19:25:54 -0700

    Initial Commit

---
+++ outline-group-demo/outline_group_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  outline_group_demoApp.swift
+//  outline-group-demo
+//
+//  Created by user on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct outline_group_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
